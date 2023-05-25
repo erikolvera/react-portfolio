a06fdef98d45c41934c4e03ff52aa0491fc596ad
erikolvera <118490997+erikolvera@users.noreply.github.com>
2023-05-24 21:53:17 -0500

    edited my contact and project file

--- eriks-react-portfolio/src/components/Project.js
+++ eriks-react-portfolio/src/components/Project.js
@@ -7,66 +7,66 @@ export default function Project() {
             id: 0,
             repo: 'https://github.com/erikolvera/note-taker',
             name: 'Note Taker',
-            description: 'This is my first project',
+            description: 'This application is a back end application that connects to a front end to create notes. The app uses express and uuid. Uuid is used to create unique IDs for each note that is created and added!',
             image: '/assets/note-taker.png',
             link: 'https://google.com',
 
         },
         {
             id: 1,
-            repo: '',
+            repo: 'https://github.com/erikolvera/team-profile-generator',
             name: 'Team Profile Generator',
-            description: 'This is my second project',
+            description: 'Using this application, you can make a quick and easy html file to make your own employee team. You will be prompted a few questions, answer them, and the html page will create itself!',
             image: '/assets/team-profile-generator.png',
             link: 'https://google.com',
 
         },
         {
             id: 2,
-            repo: '',
+            repo: 'https://github.com/erikolvera/social-network-api',
             name: 'Social Network API',
-            description: 'This is my third project',
+            description: 'This repo is an API designed for a social media app. Using the database, it can keep track of friends, your posts, see who reacted to your post and more',
             image: '/assets/social-network-api.png',
             link: 'https://google.com',
 
         },
         {
             id: 3,
-            repo: '',
+            repo: 'https://github.com/erikolvera/E-commerce-Back-End',
             name: 'E Commerce Backend',
-            description: 'This is my fourth project',
+            description: 'This application is a back end application that creates a database using Express.js and seqeulize to communicate with our MySQL databse.',
             image: '/assets/e-commerce.png',
             link: 'https://google.com',
 
         },
         {
             id: 4,
-            repo: '',
+            repo: 'https://github.com/erikolvera/code-quiz',
             name: 'Code Quiz',
-            description: 'This is my fifth project',
+            description: 'Test your knowledge on basic javascript! It is a short little quiz.',
             image: '/assets/code-quiz.png',
             link: 'https://google.com',
 
         },
         {
             id: 5,
-            repo: '',
+            repo: 'https://github.com/erikolvera/getting-wild-rescue',
             name: 'Getting Wild Resuce',
-            description: 'This is my sixth project',
+            description: 'This was my first project with a team and although it may not be the prettiest, it was a great learning experience. It is a website that would show cats and dogs that are up for adoption in your area using an API.',
             image: '/assets/getting-wild-rescue.png',
             link: 'https://google.com',
 
         }
     ]);
     return (
-        <div className="h-screen grid-container">
+        <div className=" grid-container">
             {project.map((item) => (
                 <div className="grid-item" key={item.id}>
-                    <img src={item.image} alt='placeholder' />
+                    <img src={item.image} alt='project images' />
 
                     <h2>{item.name}</h2>
                     <p>{item.description}</p>
-                    <a href={item.repo} rel="noreferrer" target='_blank'><p>Check me out!</p></a>
+                    <a href={item.repo} rel="noreferrer" target='_blank'><p>Check out my repo!</p></a>
                 </div>
             ))}
         </div>
